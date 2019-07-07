55d8717876bb3bd1c13e09db0f2b7cc88f7efcab
Bennett Foster <bennfos@gmail.com>
2019-07-06 21:12:01 -0500

    first commit

---
+++ rps.js
@@ -0,0 +1,26 @@
+const randomNum = Math.floor((Math.random() * 3) + 1);
+const rock = "Rock!";
+const paper = "Paper!";
+const scissors = "Scissors!";
+const playerInput = prompt("Rock, Paper, Scissors, Shoot! Type in your choice below");
+
+
+console.log(randomNum); 
+
+function computerPlay() {
+    if (randomNum === 1) {
+      return rock;
+    } else if (randomNum === 2) {
+        return paper;
+    } else {
+        return scissors;
+    }
+}
+
+function playRound (computerSelection, playerSelection) {
+    playerInput;
+    computerPlay()
+    if (playerInput === 
+
+}
+  
